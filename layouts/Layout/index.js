3a6488fa66132a94628fc49ce52b488375af1952
tony <hcpups0414@gmail.com>
2018-10-14 00:36:48 +0800

    add react bootstrap

---
+++ layouts/Layout/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../Header'
+
+const Layout = (props) => {
+  return (
+    <div>
+      <Header></Header>
+      <div className='container'>
+        {props.children}
+      </div>
+    </div>
+  )
+}
+
+export default Layout
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import Layout from '../layouts/Layout'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+class index extends Component {
+  state = {}
+  render() {
+    return (
+      <Layout>
+        <p>this is test</p>
+      </Layout>
+    )
+  }
+}
+export default index
